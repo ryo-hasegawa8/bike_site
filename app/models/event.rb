d060e37567e2876e061eca23df3fc28779c20484
長谷川稜 <hase08239@gmail.com>
2020-01-05 19:18:37 +0900

    delete extra line in route

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,6 @@ class Event < ApplicationRecord
   has_many :attendances, dependent: :destroy
   has_many :participants, through: :attendances, source: :user
   mount_uploader :image, ImageUploader
-  # default_scope -> { order(created_at: :desc) }
   with_options presence: true do
     validates :title
     validates :user_id
